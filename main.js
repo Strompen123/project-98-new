9dcd8537d9d6a06fc9c0140b67733baf51d9c8b0
Strompen123 <105640920+Strompen123@users.noreply.github.com>
2022-05-29 13:43:41 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+<div id="resultRow">
+
+<div id="result1">
+<img src="pic1.jpg" style="width:30%;"></img>
+</div>
+
+<div id="result2">
+    <img src="pic2.jpg" style="width:30%;"></img>
+</div>
+
+<div id="reult2">
+    <img src="pic3.jpg" style="width:30%;"></img>
+</div>
+</div>
